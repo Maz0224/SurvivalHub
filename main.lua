c678237423906ebac489e8cc2232b7d3f420ae9a
Maz0224 <amaz2.201224@gmail.com>
2026-08-20 16:43:49 -0700

    Modify window title and add Jump Power slider
    Updated the window name and subtitle, and added a Jump Power slider.

--- main.lua
+++ main.lua
@@ -1,20 +1,31 @@
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/gen2"))()
 
 local window = Rayfield:CreateWindow({
-    name = "Example",
-    subtitle = "Rayfield Gen2",
+    name = "Survival Hub",
+    subtitle = "made by maz24",
 })
 
 local basictab = window:CreateTab({ name = "Basic", icon = 93364949241311 })
 
 
 basictab:CreateSlider({
-    name = "Speed",
+    name = "Walk Speed",
     range = { 0, 120 },
     increment = 1,
     value = 16,
     suffix = "walkspeed",
     callback = function(value)
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    end,
+})
+
+basictab:CreateSlider({
+    name = "Jump Power",
+    range = { 0, 120 },
+    increment = 1,
+    value = 16,
+    suffix = "power",
+    callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
     end,
 })
